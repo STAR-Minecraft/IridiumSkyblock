bd3982b93a589b61cf2a48abc0434212a1cbf124
Peaches_MLG <PeachesMLG100@gmail.com>
2022-03-28 18:44:29 +0100

    Made IslandMembersGUI Into A PagedGUI

--- src/main/java/com/iridium/iridiumskyblock/gui/IslandMembersGUI.java
+++ src/main/java/com/iridium/iridiumskyblock/gui/IslandMembersGUI.java
@@ -1,41 +1,57 @@
 package com.iridium.iridiumskyblock.gui;
 
+import com.iridium.iridiumcore.gui.PagedGUI;
 import com.iridium.iridiumcore.Item;
 import com.iridium.iridiumcore.utils.ItemStackUtils;
+import com.iridium.iridiumcore.utils.StringUtils;
 import com.iridium.iridiumskyblock.IridiumSkyblock;
 import com.iridium.iridiumskyblock.PlaceholderBuilder;
-import com.iridium.iridiumskyblock.configs.inventories.MembersInventoryConfig;
+import com.iridium.iridiumskyblock.configs.inventories.NoItemGUI;
 import com.iridium.iridiumskyblock.database.Island;
 import com.iridium.iridiumskyblock.database.User;
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Collection;
 
 /**
  * GUI which displays all members of an Island and allows quick rank management.
  */
-public class IslandMembersGUI extends IslandGUI {
+public class IslandMembersGUI extends PagedGUI<User> {
 
-    private int page = 1;
-    private final Map<Integer, User> members;
+    private final Island island;
 
-    /**
-     * The default constructor.
-     *
-     * @param island The Island this GUI belongs to
-     */
-    public IslandMembersGUI(@NotNull Island island, Inventory previousInventory) {
-        super(IridiumSkyblock.getInstance().getInventories().membersGUI, previousInventory, island);
-        this.members = new HashMap<>();
+    public IslandMembersGUI(Island island, Inventory previousInventory) {
+        super(1,
+                IridiumSkyblock.getInstance().getInventories().membersGUI.size,
+                IridiumSkyblock.getInstance().getInventories().membersGUI.background,
+                IridiumSkyblock.getInstance().getInventories().previousPage,
+                IridiumSkyblock.getInstance().getInventories().nextPage,
+                previousInventory,
+                IridiumSkyblock.getInstance().getInventories().backButton
+        );
+        this.island = island;
+    }
+
+    @NotNull
+    @Override
+    public Inventory getInventory() {
+        NoItemGUI noItemGUI = IridiumSkyblock.getInstance().getInventories().membersGUI;
+        Inventory inventory = Bukkit.createInventory(this, getSize(), StringUtils.color(noItemGUI.title));
+        addContent(inventory);
+        return inventory;
     }
 
     @Override
+    public Collection<User> getPageObjects() {
+        return IridiumSkyblock.getInstance().getIslandManager().getIslandMembers(island);
+    }
+
     public void addContent(Inventory inventory) {
+        // TODO fix that
         inventory.clear();
         members.clear();
 
@@ -84,14 +100,29 @@ public class IslandMembersGUI extends IslandGUI {
         }
     }
 
-    /**
-     * Called when there is a click in this GUI.
-     * Cancelled automatically.
-     *
-     * @param event The InventoryClickEvent provided by Bukkit
-     */
+    @Override
+    public ItemStack getItemStack(User user) {
+        return ItemStackUtils.makeItem(IridiumSkyblock.getInstance().getInventories().membersGUI.item, new PlaceholderBuilder()
+                .applyIslandPlaceholders(island)
+                .applyPlayerPlaceholders(user)
+                .build()
+        );
+    }
+
     @Override
     public void onInventoryClick(InventoryClickEvent event) {
+        super.onInventoryClick(event);
+        User user = getItem(event.getSlot());
+        if (user == null) return;
+        switch (event.getClick()) {
+            case LEFT:
+                IridiumSkyblock.getInstance().getCommands().demoteCommand.execute(event.getWhoClicked(), new String[]{"", user.getName()});
+                break;
+            case RIGHT:
+                IridiumSkyblock.getInstance().getCommands().promoteCommand.execute(event.getWhoClicked(), new String[]{"", user.getName()});
+                break;
+        }
+        addContent(event.getInventory());
         if (members.containsKey(event.getSlot())) {
             User user = members.get(event.getSlot());
 
@@ -136,5 +167,4 @@ public class IslandMembersGUI extends IslandGUI {
             event.getWhoClicked().openInventory(new IslandBansGUI(1, getIsland(), player.getOpenInventory().getTopInventory()).getInventory());
         }
     }
-
 }
